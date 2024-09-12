7d212cc355d2b437dfefc7c9748a1da582723c2f
Muradil <muradil.koychubekob@gmail.com>
2024-09-12 16:16:17 +0600

    add docker to start project

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+      .setTitle('Book App test api')
+      .setDescription('Test work')
+      .setVersion('1.0')
+      .addTag('API')
+      .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
